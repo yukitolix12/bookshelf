6e787b96fe14032ed547d2dfcef996567b22e71d
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-08-12 15:29:10 +0900

    TitleComparator完成

--- TitleComparator.java
+++ TitleComparator.java
@@ -1,3 +1,7 @@
-public class TitleComparator {
-    
+import java.util.Comparator;
+
+public class TitleComparator implements Comparator<Book>{
+    public int compare(Book x, Book y){
+        return x.getTitle().compareTo(y.getTitle());
+    } 
 }
